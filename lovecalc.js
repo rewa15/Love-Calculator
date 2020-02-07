53adb6c7201add08164eeba3122cfbd20cd5f83e
rewa15 <rewa.kawatra@gmail.com>
2020-02-07 12:52:51 +0530

    Love Calculator

---
+++ lovecalc.js
@@ -0,0 +1,7 @@
+var name1=prompt("What is your name? ");
+var name2=prompt("What is your crush name? ");
+
+var score=Math.random()*100;
+score = Math.floor(score)+1;
+
+window.alert("Love score for "+name1+" and "+name2+" is "+ score+" % ");
